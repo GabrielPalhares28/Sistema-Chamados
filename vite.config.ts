5c2a1f6ecf49ce6bfb16ef9900ce60abd3192055
GabrielPalhares28 <gabrielpalhares764@gmail.com>
2025-01-09 19:40:03 -0300

    ajuste vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // Define a pasta de saída para o build
+  },
+  server: {
+    port: 3000, // Configura a porta do servidor de desenvolvimento (opcional)
+  },
+  base: "/", // Certifica que o caminho base está correto
 })
