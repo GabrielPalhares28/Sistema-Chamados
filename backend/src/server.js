a4f9b79b80312dbe5304f5da67fe250c632b6fb6
GabrielPalhares28 <gabrielpalhares764@gmail.com>
2025-01-09 21:10:24 -0300

    ajuste vercel2

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Middleware para habilitar CORS
 app.use(cors({
-  origin: 'http://localhost:5173', // Permitir requisições do frontend
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173', // Permitir requisições do frontend
 }));
 
 // Middleware para processar o corpo das requisições
